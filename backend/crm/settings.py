dc430e0d0f332ffcf9ff4f4601799b96f1c73707
Ramadugu Sai Kiran <ramadugusaikiran2@gmail.com>
2026-04-25 00:17:19 +0530

    Update settings.py

--- backend/crm/settings.py
+++ backend/crm/settings.py
@@ -132,7 +132,10 @@ AUTH_USER_MODEL = "common.User"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "frontend_static"),   # 🔥 ADD THIS LINE
+]
 
 # ENV_TYPE = os.environ.get("ENV_TYPE", "dev")
 ENV_TYPE = "dev"
