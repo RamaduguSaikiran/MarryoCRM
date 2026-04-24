c0fb5a104d322e0d9f785d2b075547e0f650d065
Ramadugu Sai Kiran <ramadugusaikiran2@gmail.com>
2026-04-24 23:32:21 +0530

    Change STATIC_URL to '/static/'

--- backend/crm/settings.py
+++ backend/crm/settings.py
@@ -131,7 +131,7 @@ EMAIL_BACKEND = os.environ.get(
 AUTH_USER_MODEL = "common.User"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # ENV_TYPE = os.environ.get("ENV_TYPE", "dev")
